105b42c7f2b84e2b659560a08e554d2c56a0d866
Marco Matamoros <marcomatamoros97@gmail.com>
2019-03-28 23:58:56 -0400

    Complete basic client handling

--- jobprotocol.c
+++ jobprotocol.c
@@ -1,10 +1,26 @@
-// TODO: Use this file for helper functions (especially those you want available
-// to both executables.
+#include <unistd.h>
+
+#include "jobprotocol.h"
 
 /* Example: Something like the function below might be useful
 
-   // Find and return the location of the first newline character in a string
-   // First argument is a string, second argument is the length of the string
-   int find_newline(const char *buf, int len);
-  
+// Find and return the location of the first newline character in a string
+// First argument is a string, second argument is the length of the string
+int find_newline(const char *buf, int len);
 */ 
+
+int read_to_buf(int fd, Buffer *buf) {
+    int nbytes = read(fd, buf->buf + buf->inbuf, BUFSIZE - buf->inbuf);
+    if (nbytes == -1) {
+    return -1;
+    }
+
+    buf->inbuf += nbytes;
+    buf->consumed = nbytes;
+
+    return nbytes;
+}
+
+int is_buffer_full(Buffer *buf) {
+    return buf->inbuf == BUFSIZE;
+}
--- jobserver.c
+++ jobserver.c
@@ -1,46 +1,291 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
 
 #include "socket.h"
 #include "jobprotocol.h"
 
-
 #define QUEUE_LENGTH 5
+#define MAX_CLIENTS 2
 
 #ifndef JOBS_DIR
     #define JOBS_DIR "jobs/"
 #endif
 
+// Global list of jobs
+JobList job_list;
+
+// Flag to keep track of SIGINT received
+int sigint_received;
+
+// Number of clients currently connected
+int client_count;
+
+/* SIGINT handler:
+ * We are just raising the sigint_received flag here. Our program will
+ * periodically check to see if this flag has been raised, and any necessary
+ * work will be done in main() and/or the helper functions. Write your signal 
+ * handlers with care, to avoid issues with async-signal-safety.
+ */
+void sigint_handler(int code) {
+    sigint_received = 1;
+}
+
+// TODO: SIGCHLD (child stopped or terminated) handler: mark jobs as dead
+void sigchld_handler(int code) {
+}
+
+/*
+ *  Misc
+ */
+
+/* Return the highest fd between all clients and job pipes.
+ */
+int get_highest_fd(int listen_fd, Client *clients, JobList *job_list) {
+    int max = listen_fd;
+
+    for (int i = 0; i < client_count; i++) {
+        int client_socket = clients[i].socket_fd;
+        if (client_socket > max) {
+            max = client_socket;
+        }
+    }
+
+    JobNode *current = job_list->first;
+    for (int i = 0; i < job_list->count; i++) {
+        if (current->stdout_fd > max) {
+            max = current->stdout_fd;
+        }
+
+        if (current->stderr_fd > max) {
+            max = current->stderr_fd;
+        }
+
+        current = current->next;
+    }
+
+    return max;
+}
+
+/* Frees up all memory and exits.
+ */
+void clean_exit(int listen_fd, Client *clients, JobList *job_list, int exit_status) {
+    // free(self);
+    close(listen_fd);
+    exit(exit_status);
+}
+
+/*
+ *  Client management
+ */
+
+/* Accept a connection and adds them to list of clients.
+ * Return the new client's file descriptor or -1 on error.
+ */
+int setup_new_client(int listen_fd, Client *clients) {
+    int new_fd = accept_connection(listen_fd);
+    if (new_fd < 0) {
+        return -1;
+    }
+
+    Client new_client = {new_fd};
+    clients[client_count] = new_client;
+    client_count++;
+
+    return new_fd;
+}
+
+/* Closes a client and removes it from the list of clients.
+ * Return the highest fd between all clients.
+ */
+int remove_client(int listen_fd, int client_index, Client *clients, JobList *job_list) {
+    close(clients[client_index].socket_fd);
+
+    client_count--;
+    for (int i = client_index; i < client_count; i++) {
+        clients[i] = clients[i + 1];
+    }
+
+    return get_highest_fd(listen_fd, clients, job_list);
+}
+
+/* Read message from client and act accordingly.
+ * Return their fd if it has been closed or 0 otherwise.
+ */
+int process_client_request(Client *client, JobList *job_list, fd_set *all_fds) {
+    if (is_buffer_full(&(client->buffer))) {
+        return 0;
+    }
+
+    int read_res = read_to_buf(client->socket_fd, &(client->buffer));
+    if (read_res == 0) {
+        return client->socket_fd;
+    } else if (read_res == -1) {
+        return 0;
+    }
+
+    // TODO: Handle requests in buffer
+    
+    return 0;
+}
+
+/*
+ *  Sending to client
+ */
+
+/* Write a string to a client.
+ * Returns 0 on success, 1 on failed/incomplete write, or -1 in case of error.
+ */
+int write_buf_to_client(int client_fd, char *buf, int buflen);
+
+
+/* Print message to stdout, and send network-newline message to a client.
+ * Returns 0 on success, 1 on failed/incomplete write, or -1 in case of error.
+ */
+int announce_buf_to_client(int client_fd, char *buf, int buflen);
+
+
+/* Print string to stdout, and send network-newline string to a client.
+ * Returns 0 on success, 1 on failed/incomplete write, 2 if the string
+ * is too large, or -1 in case of error.
+ */
+int announce_str_to_client(int client_fd, char* str);
+
+/* Print formatted string to stdout, and send network-newline string to a client.
+ * Returns 0 on success, 1 on failed/incomplete write, 2 if the string
+ * is too large, or -1 in case of error.
+ */
+int announce_fstr_to_client(int client_fd, const char *format, ...);
+
+
+/*
+ *  Announcing to watchers. Remember: leave the watch feature to the end.
+ */
+
+/* Print message to stdout, and send network-newline message to a list of
+ * clients. Returns 0 on success, 1 on failed/incomplete write, or -1 in
+ * case of error.
+ */
+int announce_buf_to_watchers(WatcherList *watcher_list, char *buf, int buflen);
+
+/* Print string to stdout, and send network-newline string to a list of
+ * clients. Returns 0 on success, 1 on failed/incomplete write, 2 if the
+ * string is too large, or -1 in case of error.
+ */
+int announce_str_to_watchers(WatcherList *watcher_list, char *str);
+
+/* Print formatted string to stdout, and send network-newline string to a list of
+ * clients. Returns 0 on success, 1 on failed/incomplete write, 2 if the string
+ * is too large, or -1 in case of error.
+ */
+int announce_fstr_to_watchers(WatcherList *watcher_list, const char *format, ...);
+
+/*
+ *  Childcare
+ */
+
+/* Process output from each child, remove them if they are dead, announce to watchers.
+ * Returns 1 if at least one child exists, 0 otherwise.
+ */
+int process_jobs(JobList *job_list, fd_set *current_fds, fd_set *all_fds);
+
+/* Read characters from fd and store them in buffer. Announce each message found
+ * to watchers of job_node with the given format, eg. "[JOB %d] %s\n".
+ */
+void process_job_output(JobNode *job_node, int fd, Buffer *buffer, char *format);
+
+/* Remove all dead children from job list, announce to watchers.
+ * Returns count of dead jobs removed.
+ */
+int process_dead_children(JobList *job_list, fd_set *all_fds);
+
+/* Remove the given child from the job list, announce to watchers.
+ * Returns the next node that the job pointed to.
+ */
+JobNode *process_dead_child(JobList *job_list, JobNode *dead_job, fd_set *all_fds);
+
 int main(void) {
+    // Reset SIGINT received flag.
+    sigint_received = 0;
+
     // This line causes stdout and stderr not to be buffered.
     // Don't change this! Necessary for autotesting.
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
 
+    // Set up SIGCHLD handler
+    struct sigaction sigchld_act = {{sigchld_handler}};
+    sigaction(SIGCHLD, &sigchld_act, NULL);
+    
+    // Set up SIGINT handler
+    struct sigaction sigint_act = {{sigint_handler}};
+    sigaction(SIGINT, &sigint_act, NULL);
+
+    // Set up server socket
     struct sockaddr_in *self = init_server_addr(PORT);
-    int listenfd = setup_server_socket(self, QUEUE_LENGTH);
+    int listen_fd = setup_server_socket(self, QUEUE_LENGTH);
+    if (fcntl(listen_fd, F_SETFL, O_NONBLOCK) == -1) {
+        exit(1);
+    }
 
+    // Initialize client tracking structure (array list)
+    Client clients[MAX_CLIENTS] = {0};
 
-    /* TODO: Initialize job and client tracking structures, start
-     * accepting connections. Listen for messages from both clients
-     * and jobs. Execute client commands if properly formatted. 
-     * Forward messages from jobs to appropriate clients. 
-     * Tear down cleanly.
-     */
+    // Initialize job tracking structure (linked list)
+    job_list.next = &(job_list.first);
+    
+    // Set up fd set(s) that we want to pass to select()
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(listen_fd, &readfds);
 
-    /* Here is a snippet of code to create the name of an executable
-     * to execute:
-     *
-     * char exe_file[BUFSIZE];
-     * snprintf(exe_file, BUFSIZE, "%s/%s", JOBS_DIR, <job_name>);
-     */
+    int nfds = listen_fd + 1;
+    
+    while (!sigint_received) {
+	    // Use select to wait on fds, also perform any necessary checks 
+	    // for errors or received signals
+        fd_set retread = readfds;
+        if (select(nfds, &retread, NULL, NULL, NULL) > 0) {
+            // Accept incoming connections
+            if (client_count < MAX_CLIENTS && FD_ISSET(listen_fd, &retread)) {                  int new_fd = setup_new_client(listen_fd, clients);
+                if (new_fd >= 0) {
+                    FD_SET(new_fd, &readfds);
+                    if (new_fd >= nfds) {
+                        nfds = new_fd + 1;
+                    }
+                }
+            }
+            // Check our job pipes, update max_fd if we got children
 
+            // Check on all the connected clients, process any requests
+	    // or deal with any dead connections etc.
+            for (int i = 0; i < client_count; i++) {
+                if (FD_ISSET(clients[i].socket_fd, &retread)) {
+                    int client_fd = process_client_request(clients + i, 
+                                                           &job_list, &readfds);
+                    if (client_fd > 0) {
+                        nfds = remove_client(listen_fd, i, clients, &job_list)
+                                                                            + 1;
+                    }
+                }
+            }
+         /* Here is a snippet of code to create the name of an executable
+         * to execute:
+         *
+         * char exe_file[BUFSIZE];
+         * snprintf(exe_file, BUFSIZE, "%s/%s", JOBS_DIR, <job_name>);
+         */
+        }
+    }
 
-    free(self);
-    close(listenfd);
+    clean_exit(listen_fd, clients, &job_list, 0);
     return 0;
 }
-
